f4a80a09a532029d6bef3780fdc149a2295427d3
Rudney Rodrigues <rudney.un2016@gmail.com>
2024-01-22 15:01:55 -0300

    :wrench: chore: Adding tsconfig global configuration

---
+++ packages/react/dist/index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// ../tokens/dist/index.mjs
+var colors = {
+  white: "#FFFFFF",
+  black: "#000000",
+  zinc50: "#FAFAFA",
+  zinc100: "#E1E1E6",
+  zinc200: "#C4C4CC",
+  zinc300: "#8D8D99",
+  zinc400: "#7C7C8A",
+  zinc500: "#505059",
+  zinc600: "#323238",
+  zinc700: "#29292E",
+  zinc800: "#202024",
+  zinc900: "#121214",
+  zinc950: "#09090A",
+  primary300: "#996DFF",
+  primary500: "#8257E5",
+  primary700: "#633BBC",
+  primary900: "#271A45"
+};
+
+// src/index.ts
+console.log(colors);
