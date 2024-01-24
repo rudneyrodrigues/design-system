357b011083fac33c7c22812ffc51917d68785709
Rudney Rodrigues <rudney.un2016@gmail.com>
2024-01-24 10:00:00 -0300

    :sparkles: feat: Criando primeiro stories

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,15 +1,38 @@
+import type * as Stitches from '@stitches/react'
+
 import { styled } from './styles'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   backgroundColor: '$primary300',
   color: '$zinc50',
-  borderRadius: '$full',
-  padding: '$4',
+  borderRadius: '$sm',
+  border: 'none',
+  fontWeight: '$bold',
   '&:hover': {
     backgroundColor: '$primary500',
   },
+  transition: 'background-color 0.2s ease',
+
+  variants: {
+    size: {
+      small: {
+        height: '$8',
+        padding: '0 $4',
+      },
+      medium: {
+        height: '$10',
+        padding: '0 $6',
+      },
+      large: {
+        height: '$12',
+        padding: '0 $8',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'medium',
+  },
 })
 
-export function App() {
-  return <Button>Olá mundo</Button>
-}
+export type ButtonProps = Stitches.VariantProps<typeof Button>
