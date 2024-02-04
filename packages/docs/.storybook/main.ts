471ab76271e960b0d18270757ab08a922de0f5f9
Rudney Rodrigues <rudney.un2016@gmail.com>
2024-02-04 00:25:19 -0300

    :bug: fix: Corringo o Storybook Addon Docs

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -16,7 +16,7 @@ const config: StorybookConfig = {
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-interactions"),
-    getAbsolutePath("@storybook/addon-docs"),
+    "@storybook/addon-docs",
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
