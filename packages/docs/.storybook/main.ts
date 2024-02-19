6282d351bca34c6bab74d126f492a3acbc2a9724
Rudney Rodrigues <rudney.un2016@gmail.com>
2024-02-19 13:31:56 -0300

    :wheelchair: Adicionando addon de acessibilidade no StoryBook

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -17,6 +17,7 @@ const config: StorybookConfig = {
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-interactions"),
     "@storybook/addon-docs",
+    "@storybook/addon-a11y",
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
